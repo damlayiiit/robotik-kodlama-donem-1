5600a957bc0eff45e95c2e7d17a87e0684b56fbc
damlayiiit <damlayigit10@icloud.com>
2025-02-25 14:00:14 +0300

    Add files via upload

---
+++ 2025.02.25/uygulama1/uygulama1.ino
@@ -0,0 +1,20 @@
+void setup() 
+{
+ pinMode(5,OUTPUT);
+}
+
+void loop()
+{
+   for(int i=0; i<=255; i+=10)
+  {
+    analogWrite(5,i);
+    delay(20);
+  } 
+  
+   for(int i=255; i>=0; i-=10)
+  {
+    analogWrite(5,i);
+    delay(20);
+  } 
+}
+ 
